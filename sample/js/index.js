6edbe745087ffbd029142ea357f73744327f92a3
Renzo Sala <sala@syneo-tools.com>
2019-12-18 08:50:44 +0100

    Configured sample to work with TESSERACT engine

--- sample/js/index.js
+++ sample/js/index.js
@@ -10,7 +10,7 @@ Description: demo app
 
 async function init() {
     console.log("init BCR");
-    await bcr.initialize(ocrEngines.GOOGLEVISION, cropStrategy.SMART, languages.ENGLISH, 2160, 1440, false, true);
+    await bcr.initialize(ocrEngines.TESSERACT, cropStrategy.SMART, languages.ENGLISH, 2160, 1440, false, true);
     console.log("BCR initialized");
 }
 
